5a0844278aff32dbd3cd0b946dfcf8c76d4d0d77
Zhu Zhan Yan <program.nom@gmail.com>
2019-09-21 11:59:44 +0800

    Change polling in web contest server entrypoint to host all contests.

--- containers/cms-web-contest/web-contest-entrypoint.sh
+++ containers/cms-web-contest/web-contest-entrypoint.sh
@@ -10,15 +10,8 @@ CONTEST_ID=${CMS_CONTEST_ID:-"DEFAULT"}
 POLL_INTERVAL=${CMS_POLL_INTERVAL:-"15"}
 if [ "$CONTEST_ID" = "DEFAULT" ]
 then
-    # poll until a contest has been discovered
-    while [ $? -eq 0 ]
-    do
-        # autoselect default contest
-        printf "\n" | ./scripts/cmsContestWebServer -c ALL 0
-
-        echo "ContestWebServer waiting for a contest to be created..."
-        sleep $POLL_INTERVAL
-    done
+    # configure contest web server to host all contests
+    ./scripts/cmsContestWebServer -c ALL 0
 else
     exec ./scripts/cmsContestWebServer --contest-id $CONTEST_ID 0
 fi
