c455a36a3cccdb376f3a78eced26bfb1e59629a1
Zhu Zhan Yan <program.nom@gmail.com>
2019-09-21 11:52:09 +0800

    Patched cmsProxyService to ensure it functions correctly on contest deletion

--- containers/cms-db/setup_db.sh
+++ containers/cms-db/setup_db.sh
@@ -12,5 +12,5 @@ createdb --username=postgres --owner=cmsuser cmsdb
 psql --username=postgres --dbname=cmsdb --command='ALTER SCHEMA public OWNER TO cmsuser' 
 psql --username=postgres --dbname=cmsdb --command='GRANT SELECT ON pg_largeobject TO cmsuser' 
 
-# change connection string in config to refer to localhost
 bash -c 'cd /cms && ./scripts/cmsInitDB'
+
