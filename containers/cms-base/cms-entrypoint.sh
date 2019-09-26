a0f43ed05ea7fbd0f3f68a5a20d5ad1801ae4f7a
Zhu Zhan Yan <program.nom@gmail.com>
2019-09-26 13:46:03 +0800

    Configure cms-entrypoint to check database access before running cms services

--- containers/cms-base/cms-entrypoint.sh
+++ containers/cms-base/cms-entrypoint.sh
@@ -5,16 +5,22 @@ set -e
 # Docker Entrypoint
 #
 
+## setup config
 # determine path of configuration
 CMS_CONFIG=${CMS_CONFIG:-"/cms/config/cms.conf"}
 CMS_RANKING_CONFIG=${CMS_CONFIG:-"/cms/config/cms.conf"}
-
 # populates configuration with environment values
 envsubst < $CMS_CONFIG > "/etc/$(basename $CMS_CONFIG)"
 envsubst < $CMS_RANKING_CONFIG > "/etc/$(basename $CMS_RANKING_CONFIG)"
-
 # update env vars pointing to populated config path
 CMS_CONFIG="/etc/$(basename $CMS_CONFIG)"
 CMS_RANKING_CONFIG="/etc/$(basename $CMS_RANKING_CONFIG)"
 
-exec "$@"
+# database dependency check: wait for database to start
+CMS_DB=${CMS_DB:-"localhost"}
+if [ "$CMS_DB" != localhost ]
+then
+    exec /scripts/wait-for-it.sh -s -t 10 -h $CMS_DB -p 5432 -- $@
+else
+    exec $@
+fi
