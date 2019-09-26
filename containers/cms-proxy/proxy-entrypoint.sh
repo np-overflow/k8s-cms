56902c2ea543a371c61c4774cc6f5f5a9703ab3c
Zhu Zhan Yan <program.nom@gmail.com>
2019-09-26 20:00:29 +0800

    Configure proxy entrypoint to create /tmp/healthy to indicate  health
    This is required to prevent kubernete's liveness checks from killing the
    proxy service while the proxy service waits for a content to serve.

--- containers/cms-proxy/proxy-entrypoint.sh
+++ containers/cms-proxy/proxy-entrypoint.sh
@@ -8,12 +8,19 @@ set -e
 # figure out the contest id that this proxy service targets.
 CONTEST_ID=${CMS_CONTEST_ID:-"DEFAULT"}
 POLL_INTERVAL=${CMS_POLL_INTERVAL:-"15"}
+CMS_PROXY_PORT=28600
+
+# marker file for liveness probes
+echo 1 > /tmp/is-healthy 
+
 if [ "$CONTEST_ID" = "DEFAULT" ]
 then
+    
     # poll until a contest has been discovered
     while [ $? -eq 0 ]
     do
         # autoselect default contest
+        touch /tmp/
         printf "\n" | ./scripts/cmsProxyService 0
 
         echo "Service ProxyService waiting for a contest to be created..."
@@ -22,3 +29,5 @@ then
 else
     exec ./scripts/cmsProxyService --contest-id $CONTEST_ID 0
 fi
+
+rm /tmp/is-healthy 
