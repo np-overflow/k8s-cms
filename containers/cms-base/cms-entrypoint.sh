7991f523fb639be1e5e8756fbf3925a34f4b48da
Zhu Zhan Yan <program.nom@gmail.com>
2019-09-29 22:31:22 +0800

    Make wait for database delay to be configable via CMS_DB_WAIT

--- containers/cms-base/cms-entrypoint.sh
+++ containers/cms-base/cms-entrypoint.sh
@@ -20,7 +20,8 @@ CMS_DB=${CMS_DB:-"0.0.0.0"}
 if [ "$CMS_DB" != "0.0.0.0" ] # check if not running as database
 then
     # database dependency check: wait for database to start
-    /scripts/wait-for-it.sh -t 10 -h $CMS_DB -p 5432
+    CMS_DB_WAIT=${CMS_DB_WAIT:30} # how long to wait for the database
+    /scripts/wait-for-it.sh -t $CMS_DB_WAIT -h $CMS_DB -p 5432
 
     # lose root privilege to tighten security
     exec su --preserve-environment -c "$@" cmsuser
