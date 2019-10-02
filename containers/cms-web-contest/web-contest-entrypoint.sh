268316a05e346e6ac23e26a0f8195bb8b6e6029d
Zhu Zhan Yan <program.nom@gmail.com>
2019-10-02 22:44:44 +0800

    Enforce the execution of the entrypoint script in cms-web-contest

--- containers/cms-web-contest/web-contest-entrypoint.sh
+++ containers/cms-web-contest/web-contest-entrypoint.sh
@@ -5,13 +5,20 @@ set -e
 # Contest Web Server Entrypoint
 #
 
+# determine if we are execing a command or contest web server proper
+if [ "$@" != "cmsContestWebServer" ]
+then
+    exec $@
+fi
+
+
 # figure out the contest id that this contest server targets.
 CONTEST_ID=${CMS_CONTEST_ID:-"DEFAULT"}
 POLL_INTERVAL=${CMS_POLL_INTERVAL:-"15"}
 if [ "$CONTEST_ID" = "DEFAULT" ]
 then
     # configure contest web server to host all contests
-    ./scripts/cmsContestWebServer -c ALL 0
+    /cms/scripts/cmsContestWebServer -c ALL 0
 else
-    exec ./scripts/cmsContestWebServer --contest-id $CONTEST_ID 0
+    exec /cms/scripts/cmsContestWebServer --contest-id $CONTEST_ID 0
 fi
