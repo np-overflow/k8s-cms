34bec13730a6b09668779bec05d8ca32a88fd218
Zhu Zhan Yan <program.nom@gmail.com>
2019-10-03 15:02:15 +0800

    Fixed issue where cms-entrypoint would continue to execute when cannot connect to db

--- containers/cms-base/cms-entrypoint.sh
+++ containers/cms-base/cms-entrypoint.sh
@@ -21,10 +21,14 @@ if [ "$CMS_DB" != "0.0.0.0" ] # check if not running as database
 then
     # database dependency check: wait for database to start
     CMS_DB_WAIT=${CMS_DB_WAIT:-"30"} # how long to wait for the database
-    /scripts/wait-for-it.sh -t $CMS_DB_WAIT -h $CMS_DB -p 5432
-
-    # lose root privilege to tighten security
-    exec su --preserve-environment -c "$@" cmsuser
+    if /scripts/wait-for-it.sh -t $CMS_DB_WAIT -h $CMS_DB -p 5432
+    then
+        # lose root privilege to tighten security
+        exec su --preserve-environment -c "$@" cmsuser 
+    else
+        # could not extablish database connection in time
+        exit 1
+    fi
 else
     # database requires root permissions
     exec "$@"
