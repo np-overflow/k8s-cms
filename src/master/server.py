1c6092b71bc6a2348cab7b245fc414ba9e1fe601
Zhu Zhanyan <program.nom@gmail.com>
2020-01-13 10:07:16 +0800

    Added DB healthcheck to k8s-cms master service

---
+++ src/master/server.py
@@ -0,0 +1,36 @@
+#
+# k8s-cms
+# Master Service
+#
+
+import settings
+
+from flask import Flask
+from healthcheck import HealthCheck
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
+# setup healthcheck probe
+health = HealthCheck(app, "/healthz")
+
+# health check: check connection to db
+@health.add_check
+def check_db():
+    engine = create_engine(settings.DB_CONNNECTION_STR)
+    connection = engine.connect()
+    results = connection.execute("SELECT 1")
+    value = results[0][0]
+    connection.close()
+
+    if value == 1:
+        return True, "db_ok"
+    else:
+        return False, "db_offline"
+
+# root route
+@app.route("/")
+def route_root():
+    return "k8s-cms master is up and running!"
+
+if __name__ == "__main__":
+    app.run(settings.SERVER_HOST, settings.LISTEN_PORT)
