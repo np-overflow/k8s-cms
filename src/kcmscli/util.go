28f6e9500ff1a28baa803d334cd93e36414d079f
Zhu Zhanyan <program.nom@gmail.com>
2020-01-18 04:06:29 +0800

    Added loadCSVColumn() utilties to load columns in CSVs

--- src/kcmscli/util.go
+++ src/kcmscli/util.go
@@ -54,3 +54,44 @@ func writeBytes(bytes []byte, path string) {
 	file.Close()
 }
 	
+/* CSV utilties */
+// load & return a column of the given name in the csv file located as the given csv path.
+// the file should be seperated with given seperator
+// the csv file should have a first header line with column names
+func loadCSVColumn(csvPath string, colName string, seperator string) []string {
+	// open csv 
+	file, err := os.Open(csvPath)
+	if err != nil {
+		die(err.Error())
+	}
+
+	// read header line
+	lineScanner := bufio.NewScanner(file)
+	lineScanner.Scan() 
+	header := lineScanner.Text()
+	
+	// find position colName of in the header line
+	colIdx := -1
+	colNames := strings.Split(header, seperator)
+	for i, c := range colNames {
+		if colName == c {
+			colIdx = i
+			break
+		}
+	}
+	
+	if colIdx == -1 {
+		die("loadCSVColumn: column not found in CSV file: " + colName)
+	}
+
+	// collect values for the specified column
+	var colValues []string
+	for lineScanner.Scan() {
+		line := lineScanner.Text()
+		rowValues := strings.Split(line, seperator)
+		colValues = append(colValues, rowValues[colIdx])
+	}
+	
+	return colValues
+}
+
